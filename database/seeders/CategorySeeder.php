b32671e12b60366b294e211b38624d9490b40974
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-04-28 12:32:20 -0300

    Insert register table categories - Lesson 08

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -13,6 +15,36 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            [
+                'name' => 'Celulares e Tablets',
+                'slug' => Str::slug('Celulares e Tablets'),
+                'icon' => '<i class="fas fa-mobile-alt"></i>'
+            ],
+            [
+                'name' => 'TV, Audio e Vídeo',
+                'slug' => Str::slug('TV, Audio e Vídeo'),
+                'icon' => '<i class="fas fa-tv"></i>'
+            ],
+            [
+                'name' => 'Consoles e Videogames',
+                'slug' => Str::slug('Consoles e Videogames'),
+                'icon' => '<i class="fas fa-gamepad"></i>'
+            ],
+            [
+                'name' => 'Computação',
+                'slug' => Str::slug('Computação'),
+                'icon' => '<i class="fas fa-laptop"></i>'
+            ],
+            [
+                'name' => 'Moda',
+                'slug' => Str::slug('Moda'),
+                'icon' => '<i class="fas fa-tshirt"></i>'
+            ]
+        ];
+
+        foreach($categories as $category){
+            Category::factory(1)->create($category);
+        }
     }
 }
